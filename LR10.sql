3276dd196915ece20b71abe80dac0f38332d731b
Phill <57107370+Enot238@users.noreply.github.com>
2022-05-24 22:40:20 +0300

    Update LR10.sql

--- LR10.sql
+++ LR10.sql
@@ -14,6 +14,7 @@ select *from Sum_all_sells();
 
 
 
+--  Процедура добавлення
 DROP PROCEDURE IF EXISTS add_product(IN cat int, IN nam CHAR(23), IN pro int, IN pri int);
 CREATE PROCEDURE add_product( IN cat int, IN nam CHAR(23), IN pro int, IN pri int)
 AS $$
@@ -21,5 +22,18 @@ INSERT INTO product( category, product_name, produser,price ) Values (cat,nam,pr
 $$ LANGUAGE sql;
 
 CALL add_product(1, 'Test',1 , 200);
+
+select *from product;
+
+
+
+--Процедура видалення--
+DROP PROCEDURE IF EXISTS del_product(IN i int);
+CREATE PROCEDURE del_product(IN i int)
+AS $$
+DELETE FROM product WHERE id_product=i;
+$$ LANGUAGE sql;
+
+CALL del_product(26);
 
 select *from product;
